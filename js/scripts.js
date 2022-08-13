26ed46bf241e4ac4733a2a1dcce8a7b9c7464272
Matthew Belcher <matthewjbelcher7@live.com>
2022-08-12 18:03:24 -0600

    add HTML, JS, and CSS lines that were all being saved incorrect folder

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const ul_1 = document.querySelector(".possibility1");
+const ul_2 = document.querySelector(".possibility2");
+const ul_3 = document.querySelector(".possibility3");
+const ul_4 = document.querySelector(".possibility4");
+const ul_5 = document.querySelector(".possibility5");
+
+const q1 = document.querySelector(".q1");
+const q2 = document.querySelector(".q2");
+const q3 = document.querySelector(".q3");
