04e8eb002e2987cafb4ef224b3bf8f575b962eba
Matthew Belcher <matthewjbelcher7@live.com>
2022-08-22 00:01:38 -0600

    add radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,19 @@ const q2 = document.querySelector(".q2");
 const q3 = document.querySelector(".q3");
 
 const survey = document.querySelector("")
+
+<form id="radio-form">
+  <label>
+    <input type="radio" name="flavor" value="chocolate" checked>
+    Chocolate
+  </label><br>
+  <label>
+    <input type="radio" name="flavor" value="vanilla">
+    Vanilla
+  </label><br>
+  <label>
+    <input type="radio" name="flavor" value="cookies and cream">
+    Cookies & Cream
+  </label><br>
+  <button type="submit">Submit Selection</button>
+</form>
