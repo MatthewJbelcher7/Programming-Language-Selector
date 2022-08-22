3b55000bc4ece7671e1437b7bbfbab6fb84123f8
Matthew Belcher <matthewjbelcher7@live.com>
2022-08-22 01:15:58 -0600

    WIP

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function check(){
+  
+var q1 = document.quiz.q1.value;
+var q2 = document.quiz.q1.value;
+var q3 = document.quiz.q1.value;
+var
+
+  document.getElementById('???').style.visibility = "visible";
+
+}
